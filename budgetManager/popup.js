b2722083b06be9524df384cd685643b0a38d3b17
Roshan <x0260529@ti.com>
2018-01-10 13:07:43 +0530

    Budget Manger | Added Functionalites 1. Notifications for limit and total reset 2. Options page 3. Context Menu. 4. Icon badge.
    Signed-off-by: Roshan <x0260529@ti.com>

--- budgetManager/popup.js
+++ budgetManager/popup.js
@@ -1,11 +1,12 @@
 $(function(){
 
-    chrome.storage.sync.get('total', function(budget){
+    chrome.storage.sync.get(['total', 'limit'], function(budget){
         $('#total').text(budget.total);
+        $('#limit').text(budget.limit);
     });
-    
+
     $('#spendAmount').click(function(){
-        chrome.storage.sync.get('total', function(budget){
+        chrome.storage.sync.get(['total','limit'], function(budget){
             var newTotal = 0;
             if(budget.total){
                 newTotal += parseInt(budget.total);
@@ -16,7 +17,18 @@ $(function(){
                 newTotal += parseInt(amount);
             }
 
-            chrome.storage.sync.set({'total': newTotal});
+            chrome.storage.sync.set({'total': newTotal}, function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type : 'basic',
+                        iconUrl : "icon48.png",
+                        title : "Limit Reached!",
+                        message : "Uh oh! Looks like you have reached your Limit"
+                        
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+                }
+            });
 
             $('#total').text(newTotal);
             $('#amount').val('');
